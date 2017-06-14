ea2c83629bd74df5695aee342b5051707c2df876
Scott Hollinshead <shh@mcrane.co.uk>
2017-06-14 16:44:10 +0100

    make tests pass

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,14 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/scott2619.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function(index,course){
+        var $course = $('<div />', {'class': 'course' }).appendTo('#badges');
+        $('<h3 />',{'text':course.title}).appendTo($course);
+        $('<img />',{'src':course.badge}).appendTo($course);
+        $('<a />',{'href':course.url, 'class':'btn btn-primary','text':'See Course','target':'_blank'}).appendTo($course);;
+      });
+    }
+  });
 });
